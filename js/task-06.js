399060933581c0180c6b8c823c0a595d653af93a
tomaspalica <tomasss.palica@gmail.com>
2023-05-30 16:31:39 +0200

    task 6 fixed

--- js/task-06.js
+++ js/task-06.js
@@ -2,14 +2,15 @@ const validationInput = document.querySelector("#validation-input")
 const validationNumber = validationInput.getAttribute("data-length")
 
 validationInput.addEventListener("blur", (event) => {
- if(event.currentTarget.value.length < validationNumber && event.currentTarget.value.length > 0){
+    if(event.currentTarget.value == 0 ){
+        validationInput.classList.remove("invalid")
+        validationInput.classList.remove("valid")}
+    else if(event.currentTarget.value.length < validationNumber || event.currentTarget.value.length > validationNumber){
  validationInput.classList.remove("valid")
     validationInput.classList.add("invalid")
-} else if(event.currentTarget.value.length >= validationNumber ) {  
+} else if(event.currentTarget.value.length == validationNumber ) {  
 validationInput.classList.remove("invalid")
     validationInput.classList.add("valid")
-} else {
-    validationInput.classList.remove("invalid")
-    validationInput.classList.remove("valid")
-}
+} 
+
 })
